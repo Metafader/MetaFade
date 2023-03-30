f73635dea7d04a5b29f1662057b40276643b96cd
Himanshu-kumar-gupta <himanshukumargupta135@gmail.com>
2023-03-30 21:16:27 +0530

    Added functionality to PICK IMAGE from internal storage

--- app/src/main/java/com/example/metafade/MainActivity.kt
+++ app/src/main/java/com/example/metafade/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.metafade
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.metafade.ui.theme.MetaFadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +25,99 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MetaFadeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                var selectedImageUri by remember {
+                    mutableStateOf<Uri?>(null)
                 }
+                var selectedImageUris by remember {
+                    mutableStateOf<List<Uri>>(emptyList())
+                }
+
+                val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.PickVisualMedia(),
+                    onResult = {uri -> selectedImageUri = uri }
+                )
+                
+                val multiplePhotoPickerLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.PickMultipleVisualMedia(),
+                    onResult = {uris -> selectedImageUris = uris}
+                )
+
+                MyApp(
+                    modifier = Modifier.fillMaxSize(),
+                    // Passing callbacks(functions) down for state hoisting
+                    onSingleButtonClicked = { singlePhotoPickerLauncher.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )},
+                    onMultipleButtonClicked = {multiplePhotoPickerLauncher.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )},
+                    selectedImageUri = selectedImageUri,
+                    selectedImageUris = selectedImageUris
+                )
             }
         }
     }
 }
 
+// Complete UI of App
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    onSingleButtonClicked: ()->Unit,
+    onMultipleButtonClicked: ()->Unit,
+    selectedImageUri: Uri?,
+    selectedImageUris: List<Uri>
+    ) {
+    LazyColumn(modifier.padding(horizontal = 2.dp, vertical = 2.dp)) {
+
+        // For buttons
+        item { 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Button(onClick = onSingleButtonClicked) {
+                    Text(text = "Select Single\n      Image")
+                }
+
+                Button(onClick = onMultipleButtonClicked) {
+                    Text(text = "Select Multiple\n        Image")
+                }
+            }
+        }
+
+        item {
+            AsyncImage(
+                model = selectedImageUri,
+                contentDescription = null,
+                modifier = modifier.padding(3.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+
+        items(selectedImageUris) { uri->
+            AsyncImage(
+                model = uri,
+                contentDescription = null,
+                modifier = modifier.padding(3.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun DefaultPreview() {
     MetaFadeTheme {
-        Greeting("Android")
+        MyApp(
+            onSingleButtonClicked = {}, // empty function as this is preview
+            onMultipleButtonClicked = {},
+            selectedImageUri = null,
+            selectedImageUris = emptyList()
+        )
     }
 }
